0c1b1368ee593248b6587c76f7b1ff119fb65e10
Gerben <gerbenclaqpero@gmail.com>
2026-08-16 19:03:17 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IAC_On_Prem_Exercise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
